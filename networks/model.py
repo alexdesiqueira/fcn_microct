03c44de78d7c02d2be51a02284e15e1be4039a6c
alexdesiqueira <alex.desiqueira@igdore.org>
2019-12-24 13:24:33 -0800

    Updating structure

--- networks/model.py
+++ networks/model.py
@@ -4,6 +4,8 @@ from tensorflow.keras.optimizers import Adam
 
 
 def unet(pretrained_weights=None, input_size=(512, 512, 1)):
+    """Implements the U-Net dense neural network.
+    """
     inputs = layers.Input(input_size)
     conv1 = layers.Conv2D(filters=64,
                           kernel_size=3,
@@ -132,10 +134,8 @@ def unet(pretrained_weights=None, input_size=(512, 512, 1)):
     return model
 
 
-def unet_3d(input_size=(132, 132, 116, 1)):
-    """
-
-
+def unet_3d(input_size=(64, 64, 64, 1)):
+    """Implements the 3D U-Net dense neural network.
 
     Notes
     -----
@@ -274,14 +274,16 @@ def unet_3d(input_size=(132, 132, 116, 1)):
     return model
 
 
-def tiramisu(input_size=(512, 512, 1), pool=5, growth_rate=16, dropout_p=0.2,
-             layers_per_block=[4, 5, 7, 10, 12, 15, 12, 10, 7, 5, 4]):
-    """
+def tiramisu(input_size=(512, 512, 1), n_filters=48, pool=5, growth_rate=16,
+             dropout_p=0.2, layers_per_block=[4, 5, 7, 10, 12, 15, 12, 10, 7, 5, 4]):
+    """Implements the One Hundred Layers Tiramisu dense neural network.
+
+    Notes
+    -----
     Adapted from https://github.com/smdYe/FC-DenseNet-Keras/.
     """
-    n_filters = 48  # terrible. Fix it later
     inputs = layers.Input(input_size)
-    stack = layers.Conv2D(filters=48,
+    stack = layers.Conv2D(filters=n_filters,
                           kernel_size=3,
                           padding='same',
                           kernel_initializer='he_uniform')(inputs)
@@ -297,7 +299,7 @@ def tiramisu(input_size=(512, 512, 1), pool=5, growth_rate=16, dropout_p=0.2,
             n_filters += growth_rate
 
         skip_connection.append(stack)
-        stack = TransitionDown(stack, n_filters, dropout_p=dropout_p)
+        stack = transition_down(stack, n_filters, dropout_p=dropout_p)
     skip_connection = skip_connection[::-1]
 
     # Bottleneck.
@@ -312,7 +314,7 @@ def tiramisu(input_size=(512, 512, 1), pool=5, growth_rate=16, dropout_p=0.2,
     # Upsampling path.
     for i in range(pool):
         filters_to_keep = growth_rate * layers_per_block[pool + i]
-        stack = TransitionUp(skip_connection[i], upsample_block, filters_to_keep)
+        stack = transition_up(skip_connection[i], upsample_block, filters_to_keep)
 
         upsample_block = []
         for j in range(layers_per_block[pool + i + 1]):
@@ -323,7 +325,7 @@ def tiramisu(input_size=(512, 512, 1), pool=5, growth_rate=16, dropout_p=0.2,
         upsample_block = layers.concatenate(upsample_block)
 
     # Softmax
-    output = SoftmaxLayer(stack, n_classes=1)  # is 1 correct?
+    output = softmax_layer(stack, n_classes=1)  # is 1 correct?
     model = Model(inputs, output)
 
     model.compile(optimizer=Adam(learning_rate=1e-5),  # was : 1e-4
@@ -331,3 +333,53 @@ def tiramisu(input_size=(512, 512, 1), pool=5, growth_rate=16, dropout_p=0.2,
                   metrics=['accuracy'])
 
     return model
+
+
+def BN_ReLU_Conv(inputs, n_filters, filter_size=3, dropout_p=0.2):
+    '''Apply successively Batch Normalization, ReLu nonlinearity,
+    Convolution and Dropout (if dropout_p > 0)'''
+
+    l = layers.BatchNormalization()(inputs)
+    l = layers.Activation('relu')(l)
+    l = layers.Conv2D(n_filters, filter_size, padding='same',
+                      kernel_initializer='he_uniform')(l)
+    if dropout_p != 0.0:
+        l = layers.Dropout(dropout_p)(l)
+    return l
+
+
+def transition_down(inputs, n_filters, dropout_p=0.2):
+    """ Apply first a BN_ReLu_conv layer with filter size = 1, and
+    a max pooling with a factor 2  """
+    l = BN_ReLU_Conv(inputs, n_filters, filter_size=1, dropout_p=dropout_p)
+    l = layers.MaxPooling2D((2, 2))(l)
+    return l
+
+
+def transition_up(skip_connection, block_to_upsample, n_filters_keep):
+    '''Performs upsampling on block_to_upsample by a factor 2 and
+    concatenates it with the skip_connection'''
+    # Upsample and concatenate with skip connection
+    l = layers.Conv2DTranspose(n_filters_keep,
+                               kernel_size=3,
+                               strides=2,
+                               padding='same',
+                               kernel_initializer='he_uniform')(
+                                   block_to_upsample
+                               )
+    l = layers.concatenate([l, skip_connection], axis=-1)
+    return l
+
+
+def softmax_layer(inputs, n_classes):
+    """
+    Performs 1x1 convolution followed by softmax nonlinearity
+    The output will have the shape (batch_size  * n_rows * n_cols, n_classes)
+    """
+    l = layers.Conv2D(n_classes,
+                      kernel_size=1,
+                      padding='same',
+                      kernel_initializer='he_uniform')(inputs)
+    # l = Reshape((-1, n_classes))(l)
+    l = layers.Activation('sigmoid')(l)  # or softmax for multi-class
+    return l
