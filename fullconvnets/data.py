de230fdb101f05a2ea81fa53d7c88a84a4a82e60
alexdesiqueira <alex.desiqueira@igdore.org>
2020-03-06 16:27:32 -0800

    Refactoring and documenting data. Removing test_generator

--- fullconvnets/data.py
+++ fullconvnets/data.py
@@ -6,13 +6,120 @@ import numpy as np
 import os
 
 
-def adjust_data(image, labels, num_class=2, multichannel=False):
+def train_generator(batch_size, train_path, subfolders, augmentation_dict,
+                    color_mode=('grayscale', 'grayscale'), n_classes=2,
+                    target_size=(256, 256), save_to_folder=None,
+                    save_prefix=('image', 'label'), seed=1,
+                    multichannel=False):
+    """A generator to train fully convolutional networks.
+
+    Parameters
+    ----------
+    batch_size : int
+        The batch size returned by the generator.
+    train_path : str
+        The system path for the training images.
+    subfolders : list
+        Names of image and label subfolders, stored in the train_path.
+    augmentation_dict : dict
+        Dictionary with augmentation arguments passed to the image or chunk
+        data generator.
+    color_mode : list (default : ('grayscale', 'grayscale'))
+        Names of color modes for image and label.
+    n_classes : int (default : 2)
+        Number of classes contained in labels.
+    target_size : array_like (default : (256, 256))
+        Size of the desired training windows.
+    save_to_folder : str or None (default : None)
+        If not None, the system path where the augmented images and labels
+        will be stored.
+    save_prefix : str (default : ('image', 'label'))
+        names of image and label subfolders, used when save_to_folder is not
+        None.
+    seed : int (default : 1)
+        Seed used to generate augmentations on image and label.
+    multichannel : bool (default : False)
+        False if the input images have only one channel.
+
+    Yields
+    ------
+    (image, label) : list
+        List with an augmented pair of image and label.
+
+    Notes
+    -----
+    train_generator generates image and label, setting the same seed
+    for both.
+
+    subfolders can contain a list of lists, pointing to more than one
+    subfolder. For example, we can point to two image subfolders 'image1' and
+    'image2' with subfolder=(('image1', 'image2'), 'label').
+
+    If target_size has two or three dimensions, aug_dict will be passed to
+    TensorFlow's keras.preprocessing.image.ImageDataGenerator or
+    generator_3d.ChunkDataGenerator, respectively.
+
+    References
+    ----------
+    .. [1]  https://github.com/zhixuhao/unet
+
+    Examples
+    --------
+    >>> train_gen = data.train_generator(batch_size=2,
+                                         train_path='train',
+                                         subfolders=('image', 'label'),
+                                         augmentation_dict=dict(rotation_range=0.1,
+                                                                horizontal_flip=True,
+                                                                vertical_flip=True),
+                                         target_size=(128, 128),
+                                         save_to_folder=None)
+    """
+    if len(target_size) == 2:
+        image_datagen = ImageDataGenerator(**augmentation_dict)
+        label_datagen = ImageDataGenerator(**augmentation_dict)
+    elif len(target_size) == 3:
+        image_datagen = generator_3d.ChunkDataGenerator(**augmentation_dict)
+        label_datagen = generator_3d.ChunkDataGenerator(**augmentation_dict)
+
+    # creating a generator for the input images.
+    image_gen = image_datagen.flow_from_directory(
+        train_path,
+        classes=[subfolders[0]],
+        class_mode=None,
+        color_mode=color_mode[0],
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_folder=save_to_folder,
+        save_prefix=save_prefix[0],
+        seed=seed)
+
+    # creating a generator for the input labels.
+    label_gen = label_datagen.flow_from_directory(
+        train_path,
+        classes=[subfolders[1]],
+        class_mode=None,
+        color_mode=color_mode[1],
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_folder=save_to_folder,
+        save_prefix=save_prefix[1],
+        seed=seed)
+
+    train_gen = zip(image_gen, label_gen)
+
+    for (image, label) in train_gen:
+        image, label = _adjust_data(image, label, n_classes, multichannel)
+        yield (image, label)
+
+
+def _adjust_data(image, labels, n_classes=2, multichannel=False):
+    """Helps set data according to its channels, classes, and labels."""
     if multichannel:
         image = image / 255
         labels = labels[..., 0]
-        aux_labels = np.zeros(labels.shape + (num_class,))
+        aux_labels = np.zeros(labels.shape + (n_classes,))
 
-        for num in range(num_class):
+        for num in range(n_classes):
             aux_labels[labels == num, num] = 1
 
         if multichannel:
@@ -26,77 +133,10 @@ def adjust_data(image, labels, num_class=2, multichannel=False):
             aux_labels = np.reshape(aux_labels, (rows*cols, batch))
         labels = aux_labels
 
-    elif image.max() > 1:  # terrible, but I have no options anymore
+    elif image.max() > 1:
         image = image / 255
         labels = labels / 255
         labels[labels > 0.5] = 1
         labels[labels <= 0.5] = 0
 
     return (image, labels)
-
-
-def test_generator(test_path, target_size=(256, 256), pad_width=16,
-                   multichannel=False):
-    """
-    """
-    images = io.ImageCollection(os.path.join(test_path, '*.png'))
-    for image in images:
-        image = image / 255
-        if image.shape != target_size:
-            image = transform.resize(image, target_size)
-        # padding image to correct slicing after
-        image = np.pad(image, pad_width=pad_width, mode='reflect')
-        if not multichannel:
-            image = np.reshape(image, image.shape+(1,))
-        image = np.reshape(image, (1,)+image.shape)
-
-        yield image
-
-
-def train_generator(batch_size, train_path, image_folder, label_folder,
-                    aug_dict, image_color_mode='grayscale',
-                    label_color_mode='grayscale', image_save_prefix='image',
-                    label_save_prefix='label', multichannel=False,
-                    num_class=2, save_to_dir=None, target_size=(256, 256),
-                    seed=1):
-    '''
-    can generate image and mask at the same time
-    use the same seed for image_datagen and label_datagen
-    to ensure the transformation for image and mask is the same
-    if you want to visualize the results of generator, set
-    save_to_dir = "your path"
-    '''
-    if len(target_size) == 2:
-        image_datagen = ImageDataGenerator(**aug_dict)
-        label_datagen = ImageDataGenerator(**aug_dict)
-    elif len(target_size) == 3:
-        image_datagen = generator_3d.ChunkDataGenerator(**aug_dict)
-        label_datagen = generator_3d.ChunkDataGenerator(**aug_dict)
-
-    image_gen = image_datagen.flow_from_directory(
-        train_path,
-        classes=[image_folder],
-        class_mode=None,
-        color_mode=image_color_mode,
-        target_size=target_size,
-        batch_size=batch_size,
-        save_to_dir=save_to_dir,
-        save_prefix=image_save_prefix,
-        seed=seed)
-
-    label_gen = label_datagen.flow_from_directory(
-        train_path,
-        classes=[label_folder],
-        class_mode=None,
-        color_mode=label_color_mode,
-        target_size=target_size,
-        batch_size=batch_size,
-        save_to_dir=save_to_dir,
-        save_prefix=label_save_prefix,
-        seed=seed)
-
-    train_gen = zip(image_gen, label_gen)
-
-    for (image, label) in train_gen:
-        image, label = adjust_data(image, label, num_class, multichannel)
-        yield (image, label)
