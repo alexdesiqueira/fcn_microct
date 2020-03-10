0f5fbe393a7eb9aab5fd00b992d67fa3292eff25
alexdesiqueira <alex.desiqueira@igdore.org>
2020-03-09 18:21:42 -0700

    Refactoring predict into an actual script

--- fullconvnets/predict.py
+++ fullconvnets/predict.py
@@ -1,37 +1,116 @@
 from skimage import io
 
+import argparse
 import constants as const
 import os
+import sys
 import utils
 
 
-NETWORK = 'unet_3d'
-SAMPLES = const.SAMPLES_BUNCH2
-WEIGHTS = '../coefficients/larson2019_unet_3d/larson_unet_3d.hdf5'
+def main():
+    """Main function for predict.py. Receives arguments and starts predict()."""
+    help_description = """Predict fibers on Larson et al samples using a
+    chosen neural network."""
+    help_networks = """convolutional network to be used in the
+                       training. Available networks: 'tiramisu',
+                       'tiramisu_3d', 'unet', 'unet_3d'"""
+    help_tiramisu_model = """when the used network is a tiramisu, the model to
+                             be used. Not necessary when using U-Nets.
+                             Available models: 'tiramisu-56', 'tiramisu-67'"""
+    help_weights = """file containing weight coefficients to be used on the
+                      prediction."""
+
+    # creating parser and checking arguments.
+    parser = argparse.ArgumentParser(description=help_description,
+                                     add_help=True)
+    parser.add_argument('-n',
+                        '--network',
+                        type=str,
+                        required=True,
+                        help=help_networks)
+    parser.add_argument('-t',
+                        '--tiramisu_model',
+                        type=str,
+                        required=False,
+                        help=help_tiramisu_model)
+    parser.add_argument('-w',
+                        '--weights',
+                        type=str,
+                        required=True,
+                        help=help_weights)
+    arguments = vars(parser.parse_args())
+
+    network, tiramisu_model, weights = list(arguments.values())
+
+    # checking if a tiramisu network was provided with a correct model.
+    no_tiramisu_but_tiramisu_model = (('tiramisu' not in network) and
+                                      (tiramisu_model in const.AVAILABLE_TIRAMISU_MODELS))
+    tiramisu_but_no_tiramisu_model = (('tiramisu' in network) and
+                                      (tiramisu_model not in const.AVAILABLE_TIRAMISU_MODELS))
+    if no_tiramisu_but_tiramisu_model or tiramisu_but_no_tiramisu_model:
+        parser.print_help()
+        sys.exit(0)
+
+    # starting predict().
+    predict(network, tiramisu_model, weights)
+
+    return None
 
 
-for sample in SAMPLES:
-    print(f"# Now reading sample {sample['path']}.")
-    pattern = os.path.join(sample['path'], '*' + const.EXT_SAMPLE)
-    data_sample = io.ImageCollection(load_pattern=pattern)
+def predict(network, tiramisu_model=None, weights=None):
+    """Predict fibers on Larson et al samples using a
+    chosen neural network.
 
-    print('# Processing...')
-    folder = os.path.join(utils.prediction_folder(network=NETWORK),
-                          sample['folder'])
-    utils.process_sample(folder,
-                         data=data_sample,
-                         weights=WEIGHTS,
-                         network=NETWORK)
+    Parameters
+    ----------
+    network : str
+        Network results to compare with the gold standard.
+    tiramisu_model : str or None (default : None)
+        Tiramisu model to be used.
+    batch_size : int (default : 2)
+        Size of the batches used in the training.
+    epochs : int (default : 5)
+        How many epochs are used in the training.
 
-    if sample['registered_path']:
-        pattern = os.path.join(sample['registered_path'],
-                               '*' + const.EXT_REG)
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    predict() calculates predictions on all samples in
+    constants.SAMPLES_BUNCH2 and saves the results on disk. Be sure
+    you have disk space available.
+
+    Examples
+    --------
+    >>> weights_unet_3d = '../coefficients/larson2019_unet_3d/larson_unet_3d.hdf5'
+    >>> predict(network='unet_3d', weights=weights_unet_3d')
+    """
+    for sample in const.SAMPLES_BUNCH2:
+        print(f"# Now reading sample {sample['path']}.")
+        pattern = os.path.join(sample['path'], f'*{const.EXT_SAMPLE}')
         data_sample = io.ImageCollection(load_pattern=pattern)
 
-        print('# Processing registered sample...')
-        folder = os.path.join(utils.prediction_folder(network=NETWORK),
-                              sample['folder'] + '_REG')
+        print('# Processing...')
+        folder = os.path.join(utils.prediction_folder(network,
+                                                      tiramisu_model),
+                              sample['folder'])
         utils.process_sample(folder,
                              data=data_sample,
-                             weights=WEIGHTS,
-                             network=NETWORK)
+                             weights=weights,
+                             network=network)
+
+        if sample['registered_path']:
+            pattern = os.path.join(sample['registered_path'],
+                                   '*' + const.EXT_REG)
+            data_sample = io.ImageCollection(load_pattern=pattern)
+
+            print('# Processing registered sample...')
+            folder = os.path.join(utils.prediction_folder(network,
+                                                          tiramisu_model),
+                                  f"{sample['folder']}_REG")
+            utils.process_sample(folder,
+                                 data=data_sample,
+                                 weights=weights,
+                                 network=network)
