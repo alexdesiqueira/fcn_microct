6e7220a87f7fdd2d067305151a9ec64ad5239f48
alexdesiqueira <alex.desiqueira@igdore.org>
2020-04-03 17:18:07 -0700

    Fixing 3D U-net - output segmentation map for 2-classes only

--- fullconvnets/models/unet.py
+++ fullconvnets/models/unet.py
@@ -388,18 +388,14 @@ def unet_3d(input_size=(64, 64, 64, 1)):
                               padding='same',
                               kernel_initializer='he_normal')(conv_up_1)
 
-    # output segmentation map
-    # if this is right, we need to think on the
-    # filter size when dealing with more than one class.
-    # Maybe this filter is necessary only when n_classes == 1?
-    conv_up_1 = layers.Conv3D(filters=2,
-                              kernel_size=3,
-                              activation='relu',
-                              padding='same',
-                              kernel_initializer='he_normal')(conv_up_1)
-    
     # defining last convolution.
     if n_classes == 1:
+        # output segmentation map
+        conv_up_1 = layers.Conv3D(filters=2,
+                                  kernel_size=3,
+                                  activation='relu',
+                                  padding='same',
+                                  kernel_initializer='he_normal')(conv_up_1)
         conv_output = layers.Conv3D(filters=n_classes,
                                     kernel_size=1,
                                     activation='sigmoid')(conv_up_1)
